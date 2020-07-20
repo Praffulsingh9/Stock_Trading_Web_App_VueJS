28bedbf424eec095f820de945324dc96030390de
Praffulsingh9 <prafful.singh017@gmail.com>
2020-07-20 22:51:18 +0530

    Added Individual Stock Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="container">
-        <router-view></router-view>
-    </div>
+  <div class="container">
+    <app-header></app-header>  
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
-    export default {
-
-    }
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
 <style>
-
+ body {
+     padding: 30px;
+ }
 </style>
