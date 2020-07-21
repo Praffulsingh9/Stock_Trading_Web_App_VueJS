256367a9df394c4d2502372a618a41767da3dda6
Praffulsingh9 <prafful.singh017@gmail.com>
2020-07-21 13:06:19 +0530

    Hooked portfolio component with vuex state

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -38,7 +38,7 @@ export default {
         stockPrice: this.stock.price,
         quantity: this.quantity
       };
-      this.$store.dispatch('BUY_STOCK',order); 
+      this.$store.dispatch('buyStock',order); 
       this.quantity = 0;
     }
   }
